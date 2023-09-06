605b103741f63bbf45cf3997e27332efe40db4d3
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-05 22:36:59 -0500

    starting community app

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
+  )
+}
