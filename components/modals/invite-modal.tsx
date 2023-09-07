8def02b3f4bcccd9ecbae3b7c41a2eb3334f152e
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-07 02:35:13 -0500

    updated server id layout and invite modal

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -21,7 +21,7 @@ export const InviteModal = () => {
     const [copied, setCopied] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
-    const inviteUrl = `${origin}/invite/${server?.inviteCode}}`
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`
 
     const onCopy = () => {
         navigator.clipboard.writeText(inviteUrl);
